d8adf51f1bf9c671f444fd710650019632cd6e35
hmzgt <hmzgtl16@gmail.com>
2025-08-07 19:16:52 +0100

    feat: update bottom sheet navigation and enhance layout for better user experience

--- src/app/bottom-sheet/bottom-sheet.ts
+++ src/app/bottom-sheet/bottom-sheet.ts
@@ -1,25 +1,52 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-bottom-sheet',
   imports: [
-    RouterLink
+
   ],
   templateUrl: './bottom-sheet.html',
   styleUrl: './bottom-sheet.css'
 })
 export class BottomSheet {
+
+  offersLink = '/offers';
+  casinoLink = '/casino';
+  cryptoLink = '/crypto';
+  giveawaysLink = '/giveaways';
+  ambassadorLink = '/ambassador';
   isSheetVisible: boolean = false;
   isButtonVisible: boolean = true;
+
+  constructor(private router: Router) {}
 
   showBottomSheet(): void {
     this.isSheetVisible = true;
     this.isButtonVisible = false;
   }
-
   hideBottomSheet(): void {
     this.isSheetVisible = false;
     this.isButtonVisible = true;
+  }
+  navigateToOffers() {
+    this.router.navigate([this.offersLink]);
+    this.hideBottomSheet()
+  }
+  navigateToCasino() {
+    this.router.navigate([this.casinoLink]);
+    this.hideBottomSheet()
+  }
+  navigateToCrypto() {
+    this.router.navigate([this.cryptoLink]);
+    this.hideBottomSheet()
+  }
+  navigateToGiveaways() {
+    this.router.navigate([this.giveawaysLink]);
+    this.hideBottomSheet()
+  }
+  navigateToAmbassador() {
+    this.router.navigate([this.ambassadorLink]);
+    this.hideBottomSheet()
   }
 }
--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,15 +1,45 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
-import {NgOptimizedImage} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [
-    RouterLink
-  ],
+  imports: [],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css'
 })
 export class Sidebar {
 
+  constructor(private router: Router) {
+  }
+
+  homeLink = '/home';
+  communityLink = '/community';
+  offersLink = '/offers';
+  casinoLink = '/casino';
+  cryptoLink = '/crypto';
+  giveawaysLink = '/giveaways';
+  ambassadorLink = '/ambassador';
+
+  navigateToHome(): void {
+    this.router.navigate([this.homeLink]);
+
+  }
+  navigateToCommunity() {
+    this.router.navigate([this.communityLink]);
+  }
+  navigateToOffers() {
+    this.router.navigate([this.offersLink]);
+  }
+  navigateToCasino() {
+    this.router.navigate([this.casinoLink]);
+  }
+  navigateToCrypto() {
+    this.router.navigate([this.cryptoLink]);
+  }
+  navigateToGiveaways() {
+    this.router.navigate([this.giveawaysLink]);
+  }
+  navigateToAmbassador() {
+    this.router.navigate([this.ambassadorLink]);
+  }
 }
